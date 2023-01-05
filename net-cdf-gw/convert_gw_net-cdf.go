952a303cb0aab97bc0a8e80d3bcfa12240a2b207
sanny <38938782+sannyschulz@users.noreply.github.com>
2023-01-05 18:44:21 +0100

    generate eu gw mapping

--- net-cdf-gw/convert_gw_net-cdf.go
+++ net-cdf-gw/convert_gw_net-cdf.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"image"
@@ -9,6 +11,9 @@ import (
 	"log"
 	"math"
 	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/batchatco/go-native-netcdf/netcdf"
@@ -22,6 +27,7 @@ func main() {
 	inputFile := flag.String("input", "EURASIA_WTD_annualmean.nc", "Input file")
 	startTime := flag.String("start", "2000-01-01", "Start time")
 	endTime := flag.String("end", "2010-12-31", "End time")
+	eumask := flag.String("eumask", "stu_eu_layers.csv", "EU mask")
 
 	flag.Parse()
 
@@ -189,24 +195,14 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	createGWTimeSeries(&nc, start, end, *inputFile+".png")
+	createGWTimeSeries(&nc, start, end, *inputFile+".png", *eumask)
 }
 
 // a Hermes ground water time series
 // create mapping csv file (lat, lon, gwId)
 // ground water time series file (gwId, date, value)
 // requires input time range  (start, end)
-func createGWTimeSeries(nc *api.Group, start, end time.Time, imgFileName string) {
-
-	// get lat, lon, gwId
-	type gwMapping struct {
-		gwId int
-		lat  float32
-		lon  float32
-	}
-
-	// get gwId, date, value
-	// write to csv file
+func createGWTimeSeries(nc *api.Group, start, end time.Time, imgFileName, maskFile string) {
 
 	// time
 	timeVar, err := (*nc).GetVarGetter("time")
@@ -236,21 +232,22 @@ func createGWTimeSeries(nc *api.Group, start, end time.Time, imgFileName string)
 		log.Fatal(err)
 	}
 	lenLat := latVar.Len()
-	// valsLat, err := latVar.Values()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	valsLat, err := latVar.Values()
+	if err != nil {
+		log.Fatal(err)
+	}
+	valLat := valsLat.([]float32)
 	// longitude
 	lonVar, err := (*nc).GetVarGetter("lon")
 	if err != nil {
 		log.Fatal(err)
 	}
 	lenLon := lonVar.Len()
-	// valsLon, err := lonVar.Values()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
+	valsLon, err := lonVar.Values()
+	if err != nil {
+		log.Fatal(err)
+	}
+	valLon := valsLon.([]float32)
 	// ground water
 	WTDVar, err := (*nc).GetVarGetter("WTD")
 	if err != nil {
@@ -340,44 +337,164 @@ func createGWTimeSeries(nc *api.Group, start, end time.Time, imgFileName string)
 			}
 		}
 	}
-	//valRange := math.Abs(max - min)
-	img := generatePic(int(lenLon), int(lenLat))
-	grad := colorgrad.Viridis()
-	for iLat := int64(0); iLat < lenLat; iLat++ {
-		for iLon := int64(0); iLon < lenLon; iLon++ {
-			img.Set(int(iLon), int(lenLat-iLat), ToColor(gwValues[iLat][iLon], 5, &grad))
+
+	// find start index (0) of lon and lat (may be out of range)
+	startLat, startLon := findStartLatLon(valLat, valLon, lenLat, lenLon)
+	stepLatSize := math.Abs(float64(valLat[0] - valLat[1]))
+	stepLonSize := math.Abs(float64(valLon[0] - valLon[1]))
+
+	// open file maskFile
+	sourcefile, err := os.Open(maskFile)
+	if err == nil {
+		defer sourcefile.Close()
+		// write mapping csv file
+		gwOutPath := strings.SplitN(filepath.Base(maskFile), ".", 2)[0] + "_groundwater.csv"
+		gwOut, err := os.Create(gwOutPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer gwOut.Close()
+		w := csv.NewWriter(gwOut)
+		w.Write([]string{"Column,Row,latitude,longitude,groundwater"})
+
+		lookup := make(map[GridCoord]float64)
+		firstLine := true
+		colID := -1
+		rowID := -1
+		latID := -1
+		lonID := -1
+
+		colExt := 0
+		rowExt := 0
+		scanner := bufio.NewScanner(sourcefile)
+		for scanner.Scan() {
+			line := scanner.Text()
+			tokens := strings.Split(line, ",")
+			if firstLine {
+				firstLine = false
+				// Column,Row,latitude,longitude,groundwater
+				for index, token := range tokens {
+					if token == "col" {
+						colID = index
+					}
+					if token == "row" {
+						rowID = index
+					}
+					if token == "latitude" {
+						latID = index
+					}
+					if token == "longitude" {
+						lonID = index
+					}
+				}
+			} else {
+				col, _ := strconv.ParseInt(tokens[colID], 10, 64)
+				row, _ := strconv.ParseInt(tokens[rowID], 10, 64)
+				lat, _ := strconv.ParseFloat(tokens[latID], 64)
+				lon, _ := strconv.ParseFloat(tokens[lonID], 64)
+
+				// calculate index of lat and lon in gwValues
+				iLat := ((startLat) + int64(lat/stepLatSize))
+				iLon := ((startLon) + int64(lon/stepLonSize))
+
+				// check if lat and lon of a neibor is closer, to correct rounding errors
+				iLat = isNeiborCloser(valLat, iLat, lat)
+				iLon = isNeiborCloser(valLon, iLon, lon)
+
+				gw := gwValues[iLat][iLon] * -1
+				if gw < 0 {
+					gw = 0
+				}
+				lookup[GridCoord{int(row), int(col)}] = gw
+
+				// find image extension
+				if int(col) > colExt {
+					colExt = int(col)
+				}
+				if int(row) > rowExt {
+					rowExt = int(row)
+				}
+
+				// write mapping to csv file
+				w.Write([]string{fmt.Sprintf("%s,%s,%f,%f,%1.0f", tokens[colID], tokens[rowID], lat, lon, gw)})
+
+				// print warnings if lat or lon is not the most accurate
+				prevLat := math.Abs(float64(valLat[iLat])-lat) > math.Abs(float64(valLat[iLat-1])-lat)
+				nextLat := math.Abs(float64(valLat[iLat])-lat) > math.Abs(float64(valLat[iLat+1])-lat)
+
+				if prevLat || nextLat {
+					whatOffset := 0
+					if prevLat {
+						whatOffset = -1
+					} else {
+						whatOffset = 1
+					}
+					fmt.Printf("Row_Col %d_%d - Lat Is: %v Wants: %v Prev: %v Next: %v Offset %d \n", row, col, valLat[iLat], lat, valLat[iLat-1], valLat[iLat+1], whatOffset)
+				}
+				preLon := math.Abs(float64(valLon[iLon])-lon) > math.Abs(float64(valLon[iLon-1])-lon)
+				nextLon := math.Abs(float64(valLon[iLon])-lon) > math.Abs(float64(valLon[iLon+1])-lon)
+
+				if preLon || nextLon {
+					whatOffset := 0
+					if preLon {
+						whatOffset = -1
+					} else {
+						whatOffset = 1
+					}
+					fmt.Printf("Row_Col %d_%d - Lon Is: %v Wants: %v Prev: %v Next: %v Offset %d\n", row, col, valLon[iLon], lon, valLon[iLon-1], valLon[iLon+1], whatOffset)
+				}
+			}
+		}
+		w.Flush()
+		if err := w.Error(); err != nil {
+			log.Fatal(err)
+		}
+
+		img := generatePic(colExt, rowExt)
+		gradup := colorgrad.Viridis()
+		for row := 0; row < rowExt; row++ {
+			for col := 0; col < colExt; col++ {
+				if val, ok := lookup[GridCoord{int(row), int(col)}]; ok {
+					img.Set(int(col), int(row), ToColor(val, -9999, 0, 10, &gradup))
+				} else {
+					img.Set(int(col), int(row), ToColor(-9999, -9999, 0, 10, &gradup))
+				}
+			}
+		}
+		saveImg(img, imgFileName)
+	} else {
+		// render imge without mask
+		img := generatePic(int(lenLon), int(lenLat))
+		grad := colorgrad.Viridis()
+		for iLat := int64(0); iLat < lenLat; iLat++ {
+			for iLon := int64(0); iLon < lenLon; iLon++ {
+				img.Set(int(iLon), int(lenLat-iLat), ToColor(gwValues[iLat][iLon]*(-1), -2, 0, 10, &grad))
+			}
+		}
+		saveImg(img, imgFileName)
+	}
+}
+
+type GridCoord struct {
+	row int
+	col int
+}
+
+func isNeiborCloser(arr []float32, idx int64, val float64) int64 {
+
+	if idx+1 < int64(len(arr)) {
+		next := math.Abs(float64(arr[idx])-val) > math.Abs(float64(arr[idx+1])-val)
+		if next {
+			return idx + 1
 		}
 	}
-	saveImg(img, imgFileName)
-	// // write mapping csv file
-	// f, err := os.Create("gw_mapping.csv")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer f.Close()
-	// w := csv.NewWriter(f)
-	// w.Write([]string{"gwId", "lat", "lon"})
-	// for _, mapping := range latLonMappings {
-	// 	w.Write([]string{fmt.Sprintf("%d", mapping.gwId), fmt.Sprintf("%f", mapping.lat), fmt.Sprintf("%f", mapping.lon)})
-	// }
-	// w.Flush()
-	// if err := w.Error(); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// // write time series csv file
-	// f, err = os.Create("gw_time_series.csv")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer f.Close()
-	// w = csv.NewWriter(f)
-	// w.Write([]string{"gwId", "date", "value"})
-	// for gwId, timeSlice := range uniqueGWId {
-	// 	for iTime, value := range timeSlice {
-	// 		w.Write([]string{gwId, fmt.Sprintf("%1.f", timeValues[iTime]), fmt.Sprintf("%1.2f", value)})
-	// 	}
-	// }
-	// w.Flush()
+	if idx > 0 {
+		prev := math.Abs(float64(arr[idx])-val) > math.Abs(float64(arr[idx-1])-val)
+		if prev {
+			return idx - 1
+		}
+	}
+	return idx
 }
 
 func generatePic(width, height int) *image.RGBA {
@@ -394,17 +511,54 @@ func saveImg(img *image.RGBA, imgName string) {
 	f, _ := os.Create(imgName)
 	png.Encode(f, img)
 }
-func ToColor(inVal, valRange float64, grad *colorgrad.Gradient) color.RGBA {
-	val := inVal * (-1)
-	if val < -1 {
+func ToColor(val, nodata, minValue, maxValue float64, grad *colorgrad.Gradient) color.RGBA {
+	if val == nodata {
 		return color.RGBA{188, 190, 198, 0xff} // blank
-	} else if val > valRange {
-		val = valRange
 	}
-	perc := val / valRange
+	valRange := maxValue - minValue
+	if val > maxValue {
+		val = maxValue
+	}
+	if val < minValue {
+		val = minValue
+	}
+	perc := math.Abs((val - minValue) / valRange)
 	r, g, b := grad.At(perc).RGB255()
 	texture := color.RGBA{r, g, b, 0xff} // blank
 
 	return texture
 
+}
+
+func findStartLatLon(valLat, valLon []float32, lenLat, lenLon int64) (int64, int64) {
+
+	nearestToMiddle := func(val []float32, len int64, grads float64) int64 {
+		min := math.Abs(float64(val[0]))
+		currentIdx := int64(0)
+		for i := int64(1); i < len; i++ {
+			currVal := math.Abs(float64(val[i]))
+			if currVal < min {
+				min = currVal
+				currentIdx = i
+			}
+		}
+		// calculate step size
+
+		if math.Abs(float64(valLat[currentIdx])) > 0.0001 {
+			otherIdx := currentIdx + 1
+			if otherIdx >= len {
+				otherIdx = currentIdx - 1
+			}
+			stepSize := math.Abs(float64(valLat[currentIdx] - valLat[otherIdx]))
+			numSteps := grads / stepSize
+
+			currentIdx = currentIdx - int64(math.Round(float64(valLat[currentIdx])/numSteps))
+		}
+
+		return currentIdx
+	}
+	idxLat := nearestToMiddle(valLat, lenLat, 180.0)
+	idxLon := nearestToMiddle(valLon, lenLon, 365.0)
+
+	return idxLat, idxLon
 }
